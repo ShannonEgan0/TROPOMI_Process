77dc2cb8505d76cf7f10dfac7cd07ef068420656
ShannonEgan0 <shan@egan.mobi>
2023-01-11 17:42:07 +0000

    Updating archiving code

--- tropomi_archiving.py
+++ tropomi_archiving.py
@@ -1,58 +1,87 @@
+import xmltodict
 import requests
+import sys
 import os
 
-# This first section is just cataloguing all .nc filenames from the copernicus site that have CH4 data
-Skip = 0  # Only a certain number of files can be queried at one time
-whole_list = []
-new_list = [1]
-Data_Type = "CH4"  # Target dataset, currently have tested NO2 and CH4
-Output_Folder = "archived_methane_data"
 
-# Main function
-sets=0
-while len(new_list) != 0 and sets != 3: # Will run until there are no files in the query
-    print(Skip)
-    # The URL queries the site itself, and filters it in a suitable way
-    url = "https://s5phub.copernicus.eu/dhus/odata/v1/Products?$filter=substringof(%27L2__"+Data_Type+"__%27,Name)&$orderby=Name&$select=Id,Name&$skip="+str(Skip)+"&$top=50"
-    r = requests.get(url, auth=('s5pguest','s5pguest'))
-    information = r.text
+def main():
+    limits = [(136.604, -20.725), (136.604, -16.725), (140.604, -16.725), (140.604, -20.725)]
+    files_to_download = 30
+    product_type = "L2__CH4___"
+    satellite_name = "TROPOMI"
 
-    new_list = []
-    info_list = information.split("<")
-    # Extracts just the filename from the queried data
-    for x in range(len(info_list)):
-        if str(info_list[x][0:5]) == "d:Id>":
-            temp = str(info_list[x][5:])
-        if str(info_list[x][0:7]) == "d:Name>":
-            new_list += [[str(info_list[x][7:])+".nc",temp]]
+    output_folder = "tropomi_data"
+
+    limits = bounds_to_string(limits)
+    query_results = sentinel5_query(satellite_name, product_type, limits, files_to_download)
+
+    for i in query_results:
+        download_tropomi_file(i, output_folder)
+    print("All Downloads Complete")
 
 
-    whole_list += new_list
-    Skip += 50 #Ensures the next query is with 50 new files
-    r.close()
-    sets += 1
+# Function below takes in a tuple of any length that will return to index 0 of the tuple
+# Each element of the bounds input is a pair of floating point values indicating a longitude and a latitude
+# Defines the bounds of a polygon that is used to query TROPOMI for any swathes intersecting the bounded area
+def bounds_to_string(bounds):
+    bound_text = ""
+    counter = 0
+    for i in bounds:
+        counter += 1
+        if 180 >= i[0] >= -180 or 90 >= i[0] >= -90:
+            bound_text += f"{i[0]} {i[1]},"
+        else:
+            raise Exception(f"Limits point {counter} = {i} is not within bounds of latitude and longitude")
+    bound_text += f"{bounds[0][0]} {bounds[0][1]}"
+    return bound_text
 
-print(len(whole_list))
 
-if os.path.isdir(Output_Folder):
-    pass
-else:
-    os.mkdir(Output_Folder)
+# This returns a list of length of the specified rows
+# Each item in list is dictionary with details returned from the query
+# dict_keys(['title', 'link', 'id', 'summary', 'ondemand', 'date', 'int', 'str'])
+def sentinel5_query(satellite, product, limits, rows):
+    if rows > 100:
+        raise ValueError("Number of rows must be <= 100 per TROPOMI limits")
+    url = f'https://s5phub.copernicus.eu/dhus/search?q= instrumentshortname:{satellite} AND ' \
+          f'producttype:{product} AND ' \
+          f'( footprint:"Intersects(POLYGON(({limits})))"' \
+          f')&rows={rows}&start=0'
+    query = requests.get(url, auth=('s5pguest', 's5pguest'))
+    a = xmltodict.parse(query.text)
+    list_of_results = a['feed']['entry']
+    return list_of_results
 
-#Checks download directory for all files, if a file already exists it won't be downloaded
-main_directory = os.getcwd()
-os.chdir(Output_Folder)
-Current = os.getcwd()
-Files = os.listdir(Current)
-os.chdir(main_directory + "\\" +Output_Folder)
 
-#Downloads all files previously catalogued in sequence
-for x in range(len(whole_list)):
-    if whole_list[x][0] not in Files:
-        url = "https://s5phub.copernicus.eu/dhus/odata/v1/Products('" + whole_list[x][1] + "')/$value"
-        r = requests.get(url, auth=('s5pguest','s5pguest'))
-        name = whole_list[x][0]
-        print("Downloading: " + name)
-        with open(name, 'wb') as out_file:
-            out_file.write(r.content)
-        r.close()
+def download_tropomi_file(query_item, output_folder):
+    if os.path.isdir(output_folder):
+        pass
+    else:
+        os.mkdir(output_folder)
+    current_archive = os.listdir(output_folder)
+
+    download_link = query_item['link'][0]['@href']
+    response = requests.get(download_link, auth=('s5pguest', 's5pguest'), stream=True)
+    file_name = query_item['title'] + ".nc"
+    if file_name in current_archive:
+        print(file_name, "already downloaded.")
+    else:
+        with open(output_folder + "/" + file_name, "wb") as f:
+            print(file_name, "downloading:")
+            total_length = response.headers.get('content-length')
+            if total_length is None:  # no content length header
+                f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in response.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(50 * dl / total_length)
+                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                    sys.stdout.flush()
+        print()
+    return
+
+
+if __name__ == "__main__":
+    main()
